ad3249ffb023c7583e2ee17f806e6590c65c55d0
Henri J. Norden <55378880+Henri-J-Norden@users.noreply.github.com>
2022-10-22 23:01:31 +0200

    cas

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,19 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
-do.devicecheck=1
+kernel.string=ExampleKernel cas
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=cas
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=auto;
 is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
